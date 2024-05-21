2ac74ddf40a54556de2d132c66196a4a1daea3d8
Thiago <thiago@localhost.localdomain>
2024-05-21 17:09:32 -0300

    API - Listar Usuários

---
+++ mux.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"_"`
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("users", listUserHandler)
+	http.ListenAndServe(":3000", mux)
+}
+
+func listUserHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+	rows, err := db.Query("Select * from users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	users := []User{}
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.ID, &u.Name, &u.Email); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, u)
+	}
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "users.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+}
